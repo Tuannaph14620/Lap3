afcf4ab38be0fdd218e2ea49ecf41d35970167c6
[firstname <[tuannaph14620@fpt.edu.vn]>
2022-01-14 16:28:21 +0700

    y

--- src/page/admin/news/news.js
+++ src/page/admin/news/news.js
@@ -6,10 +6,10 @@ const News = {
         <div class="flex justify-between gap-6 pt-10">
                 ${detail
         .map(
-            (post) => `
+            (post) => /* html */`
                     <div class="col basis-1/3 border-2 p-4">
-                        <a href="/news/${post.id}"><img class="w-full " src="${post.img}" alt=""></a>
-                        <a href="/news/${post.id}">
+                        <a href="/admin/news/${post.id}/edit"><img class="w-full " src="${post.img}" alt=""></a>
+                        <a href="/admin/news/${post.id}/edit">
                             <h3 class="py-2 font-bold text-red-500">${post.title}</h3>
                         </a>
                         <p>${post.desc}</p>
